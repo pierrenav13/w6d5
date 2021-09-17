87ac1a90e0ec1c54582e73ed5e40ded2c0820564
daniel <87danielbradley@gmail.com>
2021-09-17 12:17:23 -0400

    completed age

--- ninenty_nine_cats/app/models/cat.rb
+++ ninenty_nine_cats/app/models/cat.rb
@@ -1,8 +1,12 @@
+require 'date'
 class Cat < ApplicationRecord
     validates :birth_date, :name, :description, presence: true
     validates :color, inclusion: {in: %w(white black red yellow), message: 'Choose color'}
     validates :sex, inclusion: {in: %w(M F), message: 'Choose M or F'}
 
+    def age
+        (Date.today - self.birth_date).to_i/365.25
+    end
 
 
 end
